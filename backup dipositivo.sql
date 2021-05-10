eab353d33623dc166842b9a733c7fcf2506b3882
Josebimbo <59079038+Josebimbo@users.noreply.github.com>
2021-05-10 17:39:27 -0600

    Add files via upload

---
+++ backup dipositivo.sql
@@ -0,0 +1,37 @@
+USE master;  
+GO
+--Crea dispositivo de almacenamiento
+EXEC sp_addumpdevice 'disk', 'AwExamenBDII',   
+'D:\BD\AwExamenBDII.bak';  
+GO
+
+--Crear el primer backup
+BACKUP DATABASE AdventureWorks2019   
+ TO AWData 
+   WITH FORMAT, INIT, NAME = N'AW – Full Backup' ;  
+GO  
+
+BACKUP DATABASE AdventureWorks2019   
+ TO AwExamenBDII 
+   WITH FORMAT, INIT, NAME = N'AwExamenBDII – Full Backup' ;  
+GO  
+
+--Crea nuevos backups
+DECLARE @BackupName VARCHAR(100)
+SET @BackupName = N'AW – Full Backup ' + FORMAT(GETDATE(),'yyyyMMdd_hhmmss');
+
+BACKUP DATABASE AdventureWorks2019
+TO AWData
+WITH NOFORMAT, NOINIT, NAME = @BackupName,
+SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
+
+RESTORE FILELISTONLY FROM AWData
+GO
+RESTORE HEADERONLY FROM AWData
+GO
+
+SELECT      *
+FROM        sys.backup_devices
+GO
+
